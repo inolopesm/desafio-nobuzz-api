b6b751acfb9bb95665d516d2b4c6ca47b0f7250c
Matheus Lopes <inolopesm@gmail.com>
2023-11-06 16:09:45 -0300

    feat: create a nestjs project

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from "@nestjs/core";
+import { FastifyAdapter } from "@nestjs/platform-fastify";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
+  const host = process.env.HOST ?? "0.0.0.0";
+  const port = process.env.PORT ? Number.parseInt(process.env.PORT, 10) : 3000;
+  await app.listen(port, host);
+}
+
+bootstrap();
