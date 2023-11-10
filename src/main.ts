51078edd58ef2005cd1adbe1e80a06cfd7926fd6
Matheus Lopes <inolopesm@gmail.com>
2023-11-10 09:11:04 -0300

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter());
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   const host = process.env.HOST ?? "0.0.0.0";
   const port = process.env.PORT ? Number.parseInt(process.env.PORT, 10) : 3000;
   await app.listen(port, host);
